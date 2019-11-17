4a1837b54869d070d8e7d9443b85aea6ed864109
大黑 <xuyonghui157@163.com>
2019-11-17 21:33:10 +0800

    suggestion

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 
-    class Tabs{
+    //tab切换
+class Tabs{
         constructor(selector){
             this.elements = $(selector);
             this.init();
@@ -25,3 +26,43 @@
         }
     }
 
+
+class Suggestion {
+    constructor(options){
+        this.options = options;
+        this.$input = $(options.input);
+        this.$input.wrap('<div class="inputWrap"></div>');
+        this.wraper = this.$input.parent();
+        this.$ol = $('<ol class="ol-list"></ol>');
+        this.$input.after(this.$ol);
+        this.$loading = $('<div class="loading"></div>');
+        this.$loading.html(this.options.loadingTemplate);
+        this.$ol.after(this.$loading);
+        this.bindEvent();
+    }
+    bindEvent(){
+        let timerid = null;
+        this.$input.on('input',(e)=> {
+            if(timerid){
+                window.clearTimeout(timerid)
+            }
+           timerid = setTimeout(()=>{
+                    this.search(e.currentTarget.value)
+                    timerid = null;
+            },1000)
+        })
+    }
+    search(keyword){
+        $('.inputWrap').addClass('loading-star');
+        this.$ol.empty();
+
+        this.options.search(keyword,(array)=>{
+            //清除之前的ol
+
+            $('.inputWrap').removeClass('loading-star');
+            array.map((vm)=>{
+                this.$ol.append($('<li></li>').text(vm))
+            })
+        })
+    }
+}
